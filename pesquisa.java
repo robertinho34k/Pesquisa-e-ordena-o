44941e7d958b39ec6cdcdeedb79bc5067f330dcc
João Vítor Guterres Giovelli <163295551+robertinho34k@users.noreply.github.com>
2025-07-31 09:57:29 -0300

    Update pesquisa.java

--- pesquisa.java
+++ pesquisa.java
@@ -26,28 +26,4 @@ public class Aula {
 
 
 
-package aula;
-
-import java.util.ArrayList;
-
-
-public class Aula {
-
-    
-    public static void main(String[] args) {
-        ArrayList<Integer> listaNumeros = new ArrayList<>();
-        ArrayList<Integer> lista2Numeros = new ArrayList<>();
-        
-        ArrayList<String> listaPalavras = new ArrayList<>();
-        
-        Util.popularAleatorioNumeros(listaNumeros, 10 ,100 ,500);
-        Util.exibirListaNumeros(listaNumeros);
-        
-        lista2Numeros.addAll(listaNumeros);
-        
-        Util.popularAleatorioPalavras(listaPalavras, 10, 6);
-        Util.exibirListaPalavras(listaPalavras);
-    }
-    
-}
 
